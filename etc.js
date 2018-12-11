eae6bb723e0463bf711b5960807e361e06383021
Josie Lopez <josephinelopez@ufl.edu>
2018-12-11 10:36:22 -0500

    kwjfn

---
+++ etc.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+
+
+  $('#q5a1').change(function() {
+      $('#consolea').text($(this).val());
+  });
+
+  $('#q5a2').change(function() {
+      $('#consoleb').text($(this).val());
+  });
+
+  $('#q5a3').change(function() {
+      $('#consolec').text($(this).val());
+  });
+
+
+});
